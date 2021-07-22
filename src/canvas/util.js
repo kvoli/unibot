eb7f636c8b6a9afa8a812ff1c39c5ca0ac40feb4
kvoli <amcclernon@student.unimelb.edu.au>
2021-07-22 18:57:11 +1000

    refactor dir struct

--- src/canvas/util.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import dayjs from "dayjs";
-
-export const shouldPublish = (published, delayed_date) =>
-  published == undefined
-    ? true
-    : published && (delayed_date ? dayjs().isAfter(dayjs(delayed_date)) : true);
